126dd6a9d464c0ef9499a3149eb2135fc0496255
Seryozha Vaks <vaks78@gmail.com>
2025-04-06 00:20:14 +0300

    update cours added

--- src/app/services/courses-fetch.service.ts
+++ src/app/services/courses-fetch.service.ts
@@ -9,6 +9,7 @@ import {Course} from "../models/course.model";
 export class CoursesServiceWithFetch {
    private readonly COURSES_URL = environment.apiRoot + "/courses";
  
+
   async loadAllCourses(): Promise<Course[]> {
     const response = await fetch(this.COURSES_URL);
     if (!response.ok) {
@@ -17,6 +18,46 @@ export class CoursesServiceWithFetch {
     const data = await response.json();
     return data.courses;
 
+  }
+
+  async createCourse(course: Partial<Course>): Promise<Course> {
+    const response = await fetch(this.COURSES_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(course)
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok' + response.statusText);
+    }
+    return response.json();
+  } 
+
+  async editCourse(courseId: string, changes: Partial<Course>): Promise<Course> {
+    const response = await fetch(this.COURSES_URL + '/' + courseId, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(changes)
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok' + response.statusText);
+    }
+    return response.json();
+  } 
+
+  async deleteCourse(courseId: string) {
+    const response = await fetch(this.COURSES_URL + '/' + courseId, {
+      method: 'DELETE'
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok' + response.statusText);
+    }
   }
 
 }
